855f2479afc3942e16a0af497cfe479f704c2a45
hopeg19 <112434533+hopeg19@users.noreply.github.com>
2022-10-31 15:05:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 from flask import Flask, render_template
 import util
 
-# create an application instance
-# all requests it receives from clients to this object for handling
-# we are instantiating a Flask object by passing __name__ argument to the Flask constructor. 
-# The Flask constructor has one required argument which is the name of the application package. 
-# Most of the time __name__ is the correct value. The name of the application package is used 
-# by Flask to find static assets, templates and so on.
 app = Flask(__name__)
 
-# evil global variables
-# can be placed in a config file
-# here is a possible tutorial how you can do this
 username='raywu1990'
 password='test'
 host='127.0.0.1'
 port='5432'
 database='dvdrental'
 
-# route is used to map a URL with a Python function
-# complete address: ip:port/
-# 127.0.0.1:5000/
 @app.route('/')
-# this is how you define a function in Python
+
 def index():
-    # this is your index page
-    # no need to specify which type this varible is...
-    # in Python you can even do this:
-    # a = 'aaa'
-    # a = 1
-    # connect to DB
+    
     cursor, connection = util.connect_to_db(username,password,host,port,database)
     # execute SQL commands
     record = util.run_and_fetch_sql(cursor, "SELECT * from customer;")
